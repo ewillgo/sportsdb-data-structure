6d486f7be816a8ae6c7804b6a628c21aa3cbf148
liangwenwei <liangwenwei@tuandai.com>
2017-09-14 09:27:22 +0800

    Initial commit

---
+++ src/main/java/cc/sportsdb/datastructure/SportsdbDataStructureApplication.java
@@ -0,0 +1,12 @@
+package cc.sportsdb.datastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsdbDataStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsdbDataStructureApplication.class, args);
+	}
+}
---
+++ src/main/java/cc/sportsdb/datastructure/algorithm/BubbleSort.java
@@ -0,0 +1,4 @@
+package cc.sportsdb.datastructure.algorithm;
+
+public class BubbleSort {
+}
